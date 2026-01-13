58d007bfbdb6cbd7049de6d42dfe373e7f86a190
aledremoath-debug <aledremoath@gmail.com>
2026-01-14 01:32:27 +0300

    Delete core directory

--- core/resources/assets_manager.dart
+++
@@ -1,8 +0,0 @@
-const String iconPath = "assets/icons";
-
-class IconsAssets {
-  static const String logoIcon = "$iconPath/logo.png";
-  static const String logoIcon1 = "$iconPath/image1.png";
-  static const String logoIcon2 = "$iconPath/image2.png";
-  static const String logoIcon3 = "$iconPath/image3.png";
-}
